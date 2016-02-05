c630de80fdbf5690736b8bd16f7e47ad0bc58200
Prasanna <skprasannakumar92@gmail.com>
2016-02-05 21:25:03 +0530

    Change in the Sunshine app linking Array adapter with ListView

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -8,6 +8,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -50,6 +54,8 @@ public class MainActivity extends ActionBarActivity {
      */
     public static class PlaceholderFragment extends Fragment {
 
+        ArrayAdapter<String> forecastListArrayAdapter;
+
         public PlaceholderFragment() {
         }
 
@@ -57,6 +63,20 @@ public class MainActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            ArrayList<String> weekForecast = new ArrayList<>();
+            weekForecast.add("Today - Sunny - 88/63");
+            weekForecast.add("Tomorrow - Foggy - 70/46");
+            weekForecast.add("Weds - Cloudy - 72/63");
+            weekForecast.add("Thurs - Rainy - 64/51");
+            weekForecast.add("Fri - Foggy - 70/46");
+            weekForecast.add("Sat - Sunny - 76/68");
+
+            forecastListArrayAdapter = new ArrayAdapter<>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,weekForecast);
+
+            ListView forecastList = (ListView) rootView.findViewById(R.id.listview_forecast);
+            forecastList.setAdapter(forecastListArrayAdapter);
+
             return rootView;
         }
     }
